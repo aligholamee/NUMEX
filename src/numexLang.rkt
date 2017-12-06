11dc6d386ba29034beae4065a318a113d74d19fa
Ali Gholami <aligholami7596@gmail.com>
2017-12-06 14:46:25 +0330

    Converting the racketList to a numeList | issue #1

--- src/numexLang.rkt
+++ src/numexLang.rkt
@@ -14,12 +14,13 @@
 ;; definition of structures for NUMEX programs
 (struct var  (string) #:transparent)  ;; a variable
 (struct int  (num)    #:transparent)  ;; a constant number, e.g., (int 17)
+(struct bool (b)      #:transparent)  ;; a boolean value, e.g., (bool #t)
 (struct add  (e1 e2)  #:transparent)  ;; add two expressions
 (struct mult (e1 e2)  #:transparent)  ;; multiply two expressions
 (struct neg  (e1)     #:transparent)  ;; negate the expression
 (struct islthan (e1 e2) #:transparent) ;; is less than
 (struct ifzero (e1 e2 e3) #:transparent) ;; tests e1
-(struct isgthan (e1 e2 e3 e4 e5) #:transparent ;; tests if e1 is greater than e2
+(struct isgthan (e1 e2 e3 e4 e5) #:transparent) ;; tests if e1 is greater than e2
 (struct fun  (nameopt formal body) #:transparent) ;; a recursive(?) 1-argument function
 (struct call (funexp actual)       #:transparent) ;; function call
 (struct munit   ()      #:transparent) ;; unit value -- good for ending a list
@@ -31,67 +32,13 @@
 (struct closure (env fun) #:transparent) ;; a closure is not in "source" programs; it is what functions evaluate to
 
 ;; Problem 1
-(define (racketlist->numexlist xs) "CHANGE")
+(define (racketlist->numexlist xs) (if (null? xs) (munit) ;; in case the result is null -> return munit (numex null value)
+                                       (apair (if (integer? (car xs)) (int (car xs))
+                                                  (boolean? (car xs)) (bool (car xs))
+
+                                                  
+                                                                        
+
+
+
 (define (numexlist->racketlist xs) "CHANGE")
-
-;; Problem 2
-;; lookup a variable in an environment
-;; Complete this function
-(define (envlookup env str)
-  (cond [(null? env) (error "unbound variable during evaluation" str)]
-  		"CHANGE" 
-		)
-
-;; Do NOT change the two cases given to you.  
-;; DO add more cases for other kinds of NUMEX expressions.
-;; We will test eval-under-env by calling it directly even though
-;; "in real life" it would be a helper function of eval-exp.
-(define (eval-under-env e env)
-  (cond [(var? e) 
-         (envlookup env (var-string e))]
-        [(add? e) 
-         (let ([v1 (eval-under-env (add-e1 e) env)]
-               [v2 (eval-under-env (add-e2 e) env)])
-           (if (and (int? v1)
-                    (int? v2))
-               (int (+ (int-num v1) 
-                       (int-num v2)))
-               (error "NUMEX addition applied to non-number")))]
-        ;; CHANGE add more cases here
-        [#t (error (format "bad NUMEX expression: ~v" e))]))
-
-;; Do NOT change
-(define (eval-exp e)
-  (eval-under-env e null))
-        
-;; Problem 3
-
-(define (ifmunit e1 e2 e3) "CHANGE")
-
-(define (mlet* bs e2) "CHANGE")
-
-(define (ifeq e1 e2 e3 e4) "CHANGE")
-
-;; Problem 4
-
-(define numex-map "CHANGE")
-
-(define numex-mapAddN
-  (mlet "map" numex-map
-        "CHANGE (notice map is now in NUMEX scope)"))
-
-;; Challenge Problem
-
-(struct fun-challenge (nameopt formal body freevars) #:transparent) ;; a recursive(?) 1-argument function
-
-;; We will test this function directly, so it must do
-;; as described in the assignment
-(define (compute-free-vars e) "CHANGE")
-
-;; Do NOT share code with eval-under-env because that will make grading
-;; more difficult, so copy most of your interpreter here and make minor changes
-(define (eval-under-env-c e env) "CHANGE")
-
-;; Do NOT change this
-(define (eval-exp-c e)
-  (eval-under-env-c (compute-free-vars e) null))
