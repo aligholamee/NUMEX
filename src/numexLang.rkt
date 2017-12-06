b59a5bbfc327480d0cc4f257373b7534db9db107
Ali Gholami <aligholami7596@gmail.com>
2017-12-06 14:07:35 +0330

    Definition of structures for NUMEX programs was added

--- src/numexLang.rkt
+++ src/numexLang.rkt
@@ -12,30 +12,29 @@
 (provide (all-defined-out)) ;; so we can put tests in a second file
 
 ;; definition of structures for NUMEX programs
-
-;; Add the missing ones
-
+(struct var  (string) #:transparent)  ;; a variable
 (struct int  (num)    #:transparent)  ;; a constant number, e.g., (int 17)
 (struct add  (e1 e2)  #:transparent)  ;; add two expressions
-
-
+(struct mult (e1 e2)  #:transparent)  ;; multiply two expressions
+(strcut neg  (e1)     #:transparent)  ;; negate the expression
+(struct islthan (e1 e2) #:transparent) ;; is less than
+(struct ifzero (e1 e2 e3) #:transparent) ;; tests e1
+(struct isgthan (e1 e2 e3 e4 e5) #:transparent ;; tests if e1 is greater than e2
 (struct fun  (nameopt formal body) #:transparent) ;; a recursive(?) 1-argument function
 (struct call (funexp actual)       #:transparent) ;; function call
-
-
 (struct munit   ()      #:transparent) ;; unit value -- good for ending a list
 (struct ismunit (e)     #:transparent) ;; if e1 is unit then 1 else 0
-
-;; a closure is not in "source" programs; it is what functions evaluate to
-(struct closure (env fun) #:transparent) 
+(struct mlet (s e1 e2)  #:transparent) ;; a local bunder which thevalue of e1 is bound to s in the expression e2
+(struct apair (e1 e2)   #:transparent) ;; pair constructor
+(struct first (e1)      #:transparent) ;; the first element of the pair e1
+(struct second (e2)     #:transparent) ;; the second element of the pair e2
+(struct closure (env fun) #:transparent) ;; a closure is not in "source" programs; it is what functions evaluate to
 
 ;; Problem 1
-
 (define (racketlist->numexlist xs) "CHANGE")
 (define (numexlist->racketlist xs) "CHANGE")
 
 ;; Problem 2
-
 ;; lookup a variable in an environment
 ;; Complete this function
 (define (envlookup env str)
